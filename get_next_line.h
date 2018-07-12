3cb20b681d07ce921b9436160f54f273e66d07ca
dz0nda <dzonda@student.42.fr>
2018-07-12 13:45:55 +0200

    initial commit

---
+++ get_next_line.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   get_next_line.h                                  .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: dzonda <marvin@le-101.fr>                  +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2017/12/19 16:47:05 by dzonda       #+#   ##    ##    #+#       */
+/*   Updated: 2018/07/12 13:43:31 by dzonda      ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define BUFF_SIZE 3
+# include "libft/libft.h"
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+
+typedef struct		s_line
+{
+	int				fd;
+	char			*str;
+	size_t			index;
+	struct s_line	*next;
+}					t_line;
+
+int					get_next_line(const int fd, char **line);
+
+#endif
